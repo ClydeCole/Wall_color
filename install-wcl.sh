a739879c4c3b816d03d47ebf428f2b78b5e1c187
Clyde Cole <191331694+ClydeCole@users.noreply.github.com>
2025-06-29 07:15:26 +0800

    添加一鍵安裝程序，修改文件名稱，修復 wall-color-tool.sh 的小問題

---
+++ install-wcl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+copy_file(){ # copy 程式檔案目錄到bin 和 opt
+    cp ./wall-color-tool.sh /usr/bin
+    cp ./wcl-help-en.sh /opt/wcl
+    cp ./wcl-help-cn.sh /opt/wcl
+}
+
+if [[ -d /usr/bin/wall-color-tool.sh || -d /opt/wcl ]]; then # 檢索/usr/bin 和/opt/是否有wcl
+    printf "\033[31mERROR ➜\033[0mYour /usr/bin/ or /opt/ have a \"wcl\" dir\n"
+    read -r -p "do you want delete it (Y/N)" choice # 詢問是否保留
+    if [[ $choice == "Y" || $choice == "y" ]]; then # 保留
+        rm -rf /usr/bin/wcl # 如果有
+        rm -rf /opt/wcl
+        mkdir /opt/wcl
+        copy_file
+        printf "\033[1;32mInstallation\033[1;33m completed\033[0m\n"
+    else # 不保留
+        echo "Stop installation"
+    fi
+else
+    mkdir /opt/wcl # 入果沒有直接直接安裝
+    copy_file
+    printf "\033[1;32mInstallation\033[1;33m completed\033[0m\n"
+fi
+
+#刪除當前目錄文件
--- wall-color-tool.sh
+++ wall-color-tool.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-. ./wall-help-en
-. ./wall-help-cn
+. /opt/wcl/wcl-help-en.sh
+. /opt/wcl/wcl-help-cn.sh
 ############### color ###############
 COL_yellow='\033[103m'
 COL_pink='\033[48;2;255;105;180m'
@@ -20,104 +20,107 @@ NC='\033[0m'
 ############### color ###############
 
 ###############Language sitting###############
-setting(){
-if [[ "$language" = "wall-help-en" || "$language" = "wall-help-cn" ]]; then
-    : # 已設定語言，跳過
-else
-    while true; do  # 循環直至正確
-        wall -pink "Enter your language"
-        echo -e -e -n "${YELLOW}";wall -gray "1➜ English"
-        echo -e -e -n "${YELLOW}";wall -gray "2➜ Chinese"
-        printf "${YELLOW}Choose (1/2): ${BLUE}"
-        read LanguageChoice
-        echo -e "${NC}"
-        if [ "$LanguageChoice" = "1" ]; then
-            language="wall-help-en"
-            break
-        elif [ "$LanguageChoice" = "2" ]; then
-            language="wall-help-cn"
-            break
-        else
-            clear
-            wall -red "Invalid choice! Please enter 1 or 2."
-        fi
-    done
-fi
+setting() {
+    if [[ "$language" = "wcl-help-en" || "$language" = "wcl-help-cn" ]]; then
+        : # 已設定語言，跳過
+    else
+        while true; do # 循環直至正確
+            wcl -pink "Enter your language"
+            echo -e -n "${YELLOW}"
+            wcl -gray "1➜ English"
+            echo -e -n "${YELLOW}"
+            wcl -gray "2➜ Chinese"
+            echo "${YELLOW}Choose (1/2): ${BLUE}"
+            read -r LanguageChoice
+            echo -e "${NC}"
+            if [ "$LanguageChoice" = "1" ]; then
+                language="wcl-help-en"
+                break
+            elif [ "$LanguageChoice" = "2" ]; then
+                language="wcl-help-cn"
+                break
+            else
+                clear
+                wcl -red "Invalid choice! Please enter 1 or 2."
+            fi
+        done
+    fi
 }
 ############### Language sitting ###############
 
-
-
 ############### Main of Wall Color ####################
-wall() {
-if [ $# -lt 2 ] || [ "$(echo -e "$1" | cut -c 1)" != "-" ]; then
-    if [ -z "${language:-}" ]; then
-        setting # L 未定义或为空
-        ${language}
-    else
-        ${language}  # L 已定义且非空
+wcl() {
+    if [ $# -lt 2 ] || [ "$(echo -e "$1" | cut -c 1)" != "-" ]; then
+        if [ -z "${language:-}" ]; then
+            setting # L 未定义或为空
+            ${language}
+        else
+            ${language} # L 已定义且非空
+        fi
+        return 1
     fi
-    return 1
-fi
 
-# 解析參數
-color_option=$(echo -e "$1" | cut -c 2-)  # 去掉前面的短橫線
-text="$2"
+    # 解析參數
+    color_option=$(echo -e "$1" | cut -c 2-) # 去掉前面的短橫線
+    text="$2"
 
-# 檢查顏色是否存在
-bg_var="COL_${color_option}"
-eval "BG=\$$bg_var"
-if [ -z "$BG" ]; then
-    if [ -z "${language:-}" ]; then
-        setting
-        ${language}
-    else
-        ${language}
+    # 檢查顏色是否存在
+    bg_var="COL_${color_option}"
+    eval "BG=\$$bg_var"
+    if [ -z "$BG" ]; then
+        if [ -z "${language:-}" ]; then
+            setting
+            ${language}
+        else
+            ${language}
+        fi
+        return 1
     fi
-    return 1
-fi
-########################################
-calculate_width() {
-    if command -v wc >/dev/null 2>&1 && [ "$(wc -L <<EOF
+    ########################################
+    calculate_width() {
+        if command -v wc >/dev/null 2>&1 && [ "$(
+            wc -L <<EOF
 $text
 EOF
-)" -gt 0 ] 2>/dev/null; then
-        wc -L <<EOF
+        )" -gt 0 ] 2>/dev/null; then
+            wc -L <<EOF
 $text
 EOF
-        return
-    fi
+            return
+        fi
 
-    echo -e "$text" | awk '{
+        echo -e "$text" | awk '{
         width = 0;
         for (i=1; i<=length; i++) {
             c = substr($0, i, 1);
             if (c ~ /[[:print:]]/) {  # 可打印ASCII字符
-                width += 1;,
+                width += 1;
                 } else {
                 width += 2;
             }
         }
         print width;
     }'
-}
+    }
 
-get_term_width() {
-    stty size 2>/dev/null | awk '{print $2}' || echo -e 80
-}
+    get_term_width() {
+        stty size 2>/dev/null | awk '{print $2}' || echo -e 80
+    }
 
-term_width=$(get_term_width)
-text_width=$(calculate_width "$text")
+    term_width=$(get_term_width)
+    text_width=$(calculate_width "$text")
 
-left_pad=$(( (term_width - text_width) / 2 ))
-[ $left_pad -lt 0 ] && left_pad=0
-right_pad=$(( term_width - text_width - left_pad ))
-[ $right_pad -lt 0 ] && right_pad=0
+    left_pad=$(((term_width - text_width) / 2))
+    [ $left_pad -lt 0 ] && left_pad=0
+    right_pad=$((term_width - text_width - left_pad))
+    [ $right_pad -lt 0 ] && right_pad=0
 
-left_fill=$(printf "%${left_pad}s" "")
-right_fill=$(printf "%${right_pad}s" "")
+    left_fill=$(printf "%${left_pad}s" "")
+    right_fill=$(printf "%${right_pad}s" "")
 
-# 输出
-printf "%b%s%b\n" "${BG}" "${left_fill}${text}${right_fill}" "${NC}"
+    # 输出
+    printf "%b%s%b\n" "${BG}" "${left_fill}${text}${right_fill}" "${NC}"
 }
 ############### Main of Wall Color ####################
+wcl "$@"
+wcl -red "Konichiwa"

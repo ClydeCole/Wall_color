d885fccd89cf7df77515d51038e205bca7eb677d
Clyde Cole <191331694+ClydeCole@users.noreply.github.com>
2025-08-03 16:38:59 +0800

    -大幅度修改wcl主程式的內容。 -新增加functions檔案，用於放置函數 -install-wcl.sh修改了一些小問題，優化縮進 -更改了help文檔的內容

--- install-wcl.sh
+++ install-wcl.sh
@@ -6,6 +6,7 @@
 
 # 後期將設定全局安裝和用戶安裝兩個選項
 
+# 首次安裝時選擇語言
 setting_language() {
     printf "\033[1;33m 請選擇你的語言:\033[0m\n"
     printf "\033[1;32m1\033[0m. 正體中文\n"
@@ -13,11 +14,11 @@ setting_language() {
     read -r -p "輸入你的選擇 (1/2): " lang_choice
 
     if [[ $lang_choice == "1" ]]; then # 選擇中文
-        echo "cn" >> ~/.local/share/wcl/language.config
+        echo "language=\"cn\"" > ~/.local/share/wcl/settings.conf
     elif [[ $lang_choice == "2" ]]; then # 選擇英文
-        echo "en" >> ~/.local/share/wcl/language.config
+        echo "language=\"en\"" > ~/.local/share/wcl/settings.conf
     else # 都不選
-        echo "cn" >> ~/.local/share/wcl/language.config
+        echo "cn" > ~/.local/share/wcl/language.conf 
         echo "無效選擇, 預設爲繁體中文. 你可以使用命令 \"wcl -lang\" 來更改語言"
     fi
 }
@@ -31,14 +32,12 @@ copy_file() { # 用來複製檔案的函數
     chmod +x ~/.local/bin/wcl # 設定可執行權限
 }
 
-
-
-
 if [ -f ~/.local/bin/wcl ]; then # 檢查目錄是否存在
-    echo "你的系統已經安裝了WCL是否要覆蓋?(y/N)" # 如果存在
+    echo "你似乎已經安裝了WCL,是否覆蓋? 這將導致你的settings.conf配置文件被重置(y/N)" # 如果存在
     read -r choice
     if [[ $choice == "y" || $choice == "Y" ]]; then # 確認覆蓋
         rm ~/.local/bin/wcl
+        rm -rf ~/.local/~/.local/bin/wcl
         rm -rf ~/.local/share/wcl
         copy_file
         echo "WCL安裝完成"

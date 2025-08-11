98ec31b5f515e712e0b72ef7ff1f8216b8f1e883
Clyde Cole <191331694+ClydeCole@users.noreply.github.com>
2025-08-11 22:02:18 +0800

    - 重寫核心腳本並重新整理文檔
    - 主要變更：
    完全重寫`install-wcl.sh`
    徹底修改`wcl`腳本
    - 小變更：
    更新`help`說明文檔
    重新命名`color`相關變量
    - 新增：
    建立`functions`檔案以存放實用腳本
    建立`help`目錄並將幫助文檔移動到此目錄

--- install-wcl.sh
+++ install-wcl.sh
@@ -8,6 +8,7 @@
 
 # 首次安裝時選擇語言
 setting_language() {
+    echo "cn" > ~/.local/share/wcl/language.conf
     printf "\033[1;33m 請選擇你的語言:\033[0m\n"
     printf "\033[1;32m1\033[0m. 正體中文\n"
     printf "\033[1;32m2\033[0m. English\n"
@@ -24,24 +25,23 @@ setting_language() {
 }
 
 copy_file() { # 用來複製檔案的函數
-    mkdir -p ~/.local/bin ~/.local/share/wcl 
+    mkdir -p ~/.local/bin ~/.local/share/wcl
 
-    cp ./wcl ~/.local/bin/ #主程式
-    cp ./color ~/.local/share/wcl/ # 配置文件
-    cp ./wcl-help-cn ./wcl-help-en ~/.local/share/wcl/ # 幫助文檔
-    chmod +x ~/.local/bin/wcl # 設定可執行權限
+    cp -r ./help/ ./picture/ ./color ./functions ./settings.conf ~/.local/share/wcl
+    cp ./wcl ~/.local/bin/
+    chmod +x ~/.local/bin/wcl
 }
 
+
 if [ -f ~/.local/bin/wcl ]; then # 檢查目錄是否存在
-    echo "你似乎已經安裝了WCL,是否覆蓋? 這將導致你的settings.conf配置文件被重置(y/N)" # 如果存在
+    echo "你似乎已經安裝了WCL,是否繼續安裝?(y/N)" # 如果存在
     read -r choice
-    if [[ $choice == "y" || $choice == "Y" ]]; then # 確認覆蓋
+    if [[ $choice == "y" || $choice == "Y" ]]; then # 確認安裝
         rm ~/.local/bin/wcl
-        rm -rf ~/.local/~/.local/bin/wcl
         rm -rf ~/.local/share/wcl
         copy_file
         echo "WCL安裝完成"
-    else # 取消覆蓋
+    else # 取消安裝
         echo "停止安裝"
         exit 0
     fi
@@ -50,4 +50,7 @@ else
     echo "WCL安裝完成"
 fi
 
+bash ~/.local/bin/wcl -gray "如果你沒有將~/.local/bin添加進你的.rc文檔內"
+bash ~/.local/bin/wcl -gray "請在你的終端鍵入(注意, 將bashrc更換成你的run command文檔名稱):"
+bash ~/.local/bin/wcl -gray "echo \'export PATH=\"\$HOME/.local/bin:\$PATH\"\' >> ~/.bashrc && source ~/.bashrc"
 setting_language
